80e16ff2fe093edfb439b597533b860dcbfd70f8
Kim Jun Su <64389254+kofhoom@users.noreply.github.com>
2023-09-18 16:37:33 +0900

    분기패포3

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -10,7 +10,7 @@ export default function BoardsPage(){
         </>
     ) 
 }
-export const getServerSideProps= () =>{
+// export const getServerSideProps= () =>{
     //만약 서버사이드 렌더링을 하는 페이지 라면?? => out 폴더로 생성 불가
     //                                         => 이런경우, next.config.jsㅔㅇ서 exportPathMap으로 현재페이지 제외시키기
-}
+// }
