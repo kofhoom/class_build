88c39380383d27c0f48d8ab0720f08d8c9cf89a9
Kim Jun Su <64389254+kofhoom@users.noreply.github.com>
2023-09-18 16:34:32 +0900

    분기패포

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  generateBuildId:()=>"deploy-project",
+  // 아래 주소들만 out폴더에 만들어줘 => getServerSideProps 있는 페이지는 제외
+  exportPathMap:()=>({
+    "/":{page:"/"},
+    "/boards":{page:"/boards"},
+    "/404":{page:"/404"},
+  })
 }
 
 module.exports = nextConfig
